c59ce09dfe69f853c53cd7c30387047eded4bde2
OMouta <tiagomouta609@gmail.com>
2025-03-10 21:56:59 +0000

    update main entry point and remove unused index file

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * SSAL - Super Simple Automating Language
- */
-
-// Export the main components
-export { parseSSAL, parseSSALContent } from './parser.js';
-export { Executor } from './executor.js';
-export * from './utils.js';
